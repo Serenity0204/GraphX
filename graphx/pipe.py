c853ffc9db9dd3161b992a57d6a930e7e649a169
serenity0204 <yuhenglin0204@gmail.com>
2023-09-12 15:16:23 -0700

    added filter and exclude

--- graphx/pipe.py
+++ graphx/pipe.py
@@ -41,7 +41,6 @@ class Pipe:
         for arg in args:
             if arg not in seen:
                 seen.add(arg)
-
                 result.append(arg)
         return result
 
@@ -52,17 +51,27 @@ class Pipe:
         for arg in args:
             if count == num:
                 break
-
             result.append(arg)
-
             count += 1
         return result
 
+    def _filter(self, args: List[Vertex], value, mode) -> List[Vertex]:
+        result = []
+        for arg in args:
+            if type(arg.values()) != type(value):
+                raise TypeError("type is not comparable")
+            if value == arg.values() and mode == "filter":
+                result.append(arg)
+            if value != arg.values() and mode == "exclude":
+                result.append(arg)
+        return result
+
     def filter(self, args: List[Vertex], value) -> List[Vertex]:
-        pass
+        return self._filter(args, value, "filter")
 
     def exclude(self, args: List[Vertex], value) -> List[Vertex]:
-        pass
+        return self._filter(args, value, "exclude")
 
-    def sort(self, args: List[Vertex], value, ascending=True) -> List[Vertex]:
+    ## value has to be sortable
+    def sort(self, args: List[Vertex], ascending=True) -> List[Vertex]:
         pass
