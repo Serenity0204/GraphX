58a6957d0bac6d4d0b85aeed04ec9cc1fc77e56f
Yu-Heng Lin <yuhenglin0204@gmail.com>
2023-10-18 00:21:04 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="graphx",
-    version="0.1",
+    name="linnngraphx",
+    version="0.2",
     packages=find_packages(),
+    package_dir={"linnngraphx": "graphx"},
     install_requires=[],
+    description="GraphX is a Python-based in-memory graph storage engine designed to facilitate the representation of complex relational queries, enabling users to store data while effectively capturing relationships between individual data points.",
 )
